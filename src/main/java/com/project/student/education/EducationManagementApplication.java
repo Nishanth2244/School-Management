6a585dfb5ddaa6efd421df572d4eb7cd5cb2c8a2
Nishanth2244 <thotanishanthnetha222@gmail.com>
2026-06-04 18:12:20 +0530

    EnabledScheduling@

--- src/main/java/com/project/student/education/EducationManagementApplication.java
+++ src/main/java/com/project/student/education/EducationManagementApplication.java
@@ -3,10 +3,11 @@ package com.project.student.education;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 
-
+@EnableScheduling
 @SpringBootApplication
 public class EducationManagementApplication {
 
