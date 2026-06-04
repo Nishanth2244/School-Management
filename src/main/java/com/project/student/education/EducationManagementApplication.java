da287b4ea1e3294da8425c89d112ac4d2e08bcae
Nishanth2244 <thotanishanthnetha222@gmail.com>
2026-06-04 17:32:43 +0530

    Schedular

--- src/main/java/com/project/student/education/EducationManagementApplication.java
+++ src/main/java/com/project/student/education/EducationManagementApplication.java
@@ -2,6 +2,8 @@ package com.project.student.education;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 
@@ -13,4 +15,8 @@ public class EducationManagementApplication {
         SpringApplication.run(EducationManagementApplication.class, args);
     }
 
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
---
+++ src/main/java/com/project/student/education/config/KeepAliveScheduler.java
@@ -0,0 +1,29 @@
+package com.project.student.education.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Component
+public class KeepAliveScheduler {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String myUrl = "https://school-management-crba.onrender.com/api/driver/all";
+
+    @Scheduled(fixedRate = 600000)
+    public void keepAlive() {
+        try {
+            restTemplate.getForObject(myUrl, String.class);
+            System.out.println("Keep-alive ping sent at " + java.time.LocalDateTime.now());
+        } catch (Exception e) {
+            System.err.println("Ping failed: " + e.getMessage());
+        }
+    }
+}   
